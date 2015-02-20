f05a6563c2444cb4214ceaa061538861c0aca14f
Bertie Scott <bertie@bertie.io>
2015-02-20 11:35:13 +0000

    Update and rename TempSensor.py to PiDisplay.php

---
+++ PiDisplay.php
@@ -0,0 +1,17 @@
+<?php 
+
+$Pi = file_get_contents('http://bertie.io/Pi.txt');
+$len = rand(1,20);
+$pos = rand(0,strlen($Pi) - $len);
+
+$out = substr($Pi, $pos, $len);
+
+echo "The ";
+echo  $len;
+echo  " decimals at ";
+echo $pos;
+echo " out of 4194294 ";
+echo " are ";
+echo $out;
+
+?>
